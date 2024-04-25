c8008184edf6228670db703e4e0896d9381b24c4
Dávid Schmidt <48670893+DavidSchmidt3@users.noreply.github.com>
2024-04-25 09:58:41 +0200

    Initial commit

---
+++ internal/ambulance_wl/impl_ambulance_waiting_list_test.go
@@ -0,0 +1,128 @@
+package ambulance_wl
+
+import (
+	"context"
+	"net/http/httptest"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/DavidSchmidt3/ambulance-webapi/internal/db_service"
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/suite"
+)
+
+type AmbulanceWlSuite struct {
+	suite.Suite
+	dbServiceMock *DbServiceMock[Ambulance]
+}
+
+func TestAmbulanceWlSuite(t *testing.T) {
+	suite.Run(t, new(AmbulanceWlSuite))
+}
+
+type DbServiceMock[DocType interface{}] struct {
+	mock.Mock
+}
+
+func (this *DbServiceMock[DocType]) CreateDocument(ctx context.Context, id string, document *DocType) error {
+	args := this.Called(ctx, id, document)
+	return args.Error(0)
+}
+
+func (this *DbServiceMock[DocType]) FindDocument(ctx context.Context, id string) (*DocType, error) {
+	args := this.Called(ctx, id)
+	return args.Get(0).(*DocType), args.Error(1)
+}
+
+func (this *DbServiceMock[DocType]) UpdateDocument(ctx context.Context, id string, document *DocType) error {
+	args := this.Called(ctx, id, document)
+	return args.Error(0)
+}
+
+func (this *DbServiceMock[DocType]) DeleteDocument(ctx context.Context, id string) error {
+	args := this.Called(ctx, id)
+	return args.Error(0)
+}
+
+func (this *DbServiceMock[DocType]) Disconnect(ctx context.Context) error {
+	args := this.Called(ctx)
+	return args.Error(0)
+}
+
+func (suite *AmbulanceWlSuite) SetupTest() {
+	suite.dbServiceMock = &DbServiceMock[Ambulance]{}
+
+	// Compile time Assert that the mock is of type db_service.DbService[Ambulance]
+	var _ db_service.DbService[Ambulance] = suite.dbServiceMock
+
+	suite.dbServiceMock.
+		On("FindDocument", mock.Anything, mock.Anything).
+		Return(
+			&Ambulance{
+				Id: "test-ambulance",
+				WaitingList: []WaitingListEntry{
+					{
+						Id:                       "test-entry",
+						PatientId:                "test-patient",
+						WaitingSince:             time.Now(),
+						EstimatedDurationMinutes: 101,
+					},
+				},
+			},
+			nil,
+		)
+}
+
+func (suite *AmbulanceWlSuite) Test_UpdateWl_DbServiceUpdateCalled() {
+	// ARRANGE
+	suite.dbServiceMock.
+		On("UpdateDocument", mock.Anything, mock.Anything, mock.Anything).
+		Return(nil)
+
+	json := `{
+		"id": "test-entry",
+		"patientId": "test-patient",
+		"estimatedDurationMinutes": 42
+	}`
+
+	gin.SetMode(gin.TestMode)
+	recorder := httptest.NewRecorder()
+	ctx, _ := gin.CreateTestContext(recorder)
+	ctx.Set("db_service", suite.dbServiceMock)
+	ctx.Params = []gin.Param{
+		{Key: "ambulanceId", Value: "test-ambulance"},
+		{Key: "entryId", Value: "test-entry"},
+	}
+	ctx.Request = httptest.NewRequest("POST", "/ambulance/test-ambulance/waitinglist/test-entry", strings.NewReader(json))
+
+	sut := implAmbulanceWaitingListAPI{}
+	// ACT
+	sut.UpdateWaitingListEntry(ctx)
+
+	// ASSERT
+	suite.dbServiceMock.AssertCalled(suite.T(), "UpdateDocument", mock.Anything, "test-ambulance", mock.Anything)
+}
+
+func (suite *AmbulanceWlSuite) Test_GetWlEntry_EntryFound_ReturnsEntry() {
+	// ARRANGE
+	gin.SetMode(gin.TestMode)
+	recorder := httptest.NewRecorder()
+	ctx, _ := gin.CreateTestContext(recorder)
+	ctx.Set("db_service", suite.dbServiceMock)
+	ctx.Params = []gin.Param{
+		{Key: "ambulanceId", Value: "test-ambulance"},
+		{Key: "entryId", Value: "test-entry"},
+	}
+
+	sut := implAmbulanceWaitingListAPI{}
+	// ACT
+	sut.GetWaitingListEntry(ctx)
+
+	// ASSERT
+	suite.Equal(200, recorder.Code)
+	suite.Contains(recorder.Body.String(), "test-entry")
+
+	suite.dbServiceMock.AssertCalled(suite.T(), "FindDocument", mock.Anything, "test-ambulance")
+}
---
+++ scripts/run.ps1
@@ -0,0 +1,32 @@
+param (
+    $command
+)
+
+if (-not $command)  {
+    $command = "start"
+}
+
+$ProjectRoot = "${PSScriptRoot}/.."
+
+$env:AMBULANCE_API_ENVIRONMENT="Development"
+$env:AMBULANCE_API_PORT="8080"
+$env:AMBULANCE_API_MONGODB_USERNAME="root"
+$env:AMBULANCE_API_MONGODB_PASSWORD="neUhaDnes"
+
+switch ($command) {
+    "start" {
+        go run ${ProjectRoot}/cmd/ambulance-api-service
+    }
+    "openapi" {
+        docker run --rm -ti -v ${ProjectRoot}:/local openapitools/openapi-generator-cli generate -c /local/scripts/generator-cfg.yaml
+    }
+    "docker" {
+        docker build -t davidschmidt38/ambulance-wl-webapi:local-build -f ${ProjectRoot}/build/docker/Dockerfile .
+    }
+    "test" {
+        go test -v ./...
+    }
+    default {
+        throw "Unknown command: $command"
+    }
+}
