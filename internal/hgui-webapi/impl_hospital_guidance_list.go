8accd0946934dc059e921b97cff17dcb2326c377
Dávid Schmidt <48670893+DavidSchmidt3@users.noreply.github.com>
2024-05-09 14:31:40 +0200

    fix

--- internal/hgui-webapi/impl_hospital_guidance_list.go
+++ internal/hgui-webapi/impl_hospital_guidance_list.go
@@ -24,7 +24,7 @@ func (this *implHospitalGuidanceListAPI) CreateHospitalGuidance(ctx *gin.Context
 
 	// print
 	fmt.Println(value)
-	db, ok := value.(*db_service.DbService[GuidanceEntry])
+	db, ok := value.(db_service.DbService[GuidanceEntry])
 	fmt.Println(db, ok)
 	if !ok {
 		ctx.JSON(
