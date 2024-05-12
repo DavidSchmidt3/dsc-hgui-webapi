4ca1e3f82ed35bb702df35e5fbb6a759ec8e0216
Dávid Schmidt <48670893+DavidSchmidt3@users.noreply.github.com>
2024-05-12 12:04:30 +0200

    test

--- deployments/kustomize/install/params/init-db.js
+++ deployments/kustomize/install/params/init-db.js
@@ -41,5 +41,22 @@ db.createCollection(collection);
 // create indexes
 db[collection].createIndex({ "id": 1 });
 
+let result = db[collection].insertMany([
+  {
+    "id": "774d8d43-3984-4bf0-9261-e360b61213ec",
+    "patientName": "John Doe",
+    "title": "Test problem",
+    "description": "Test description",
+    "severity": "low",
+    "createdAt": new Date(),
+    "email": "test@test.com"
+  },
+]);
+
+if (result.writeError) {
+  console.error(result);
+  print(`Error when writing the data: ${result.errmsg}`);
+}
+
 // exit with success
 process.exit(0);
